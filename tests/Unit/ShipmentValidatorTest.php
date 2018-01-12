a0df7bf7864a0d597fe91a2446197d2ba23db868
Nick de Vries <nick@myparcel.com>
2018-01-12 12:25:22 +0100

    :white_check_mark: Wrote a test for the ShipmentValidator class.

---
+++ tests/Unit/ShipmentValidatorTest.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace MyParcelCom\ApiSdk\Tests\Unit;
+
+use MyParcelCom\ApiSdk\Resources\Address;
+use MyParcelCom\ApiSdk\Resources\Contract;
+use MyParcelCom\ApiSdk\Resources\Service;
+use MyParcelCom\ApiSdk\Resources\Shipment;
+use MyParcelCom\ApiSdk\Resources\Shop;
+use MyParcelCom\ApiSdk\Utils\StringUtils;
+use MyParcelCom\ApiSdk\Validators\ShipmentValidator;
+use PHPUnit\Framework\TestCase;
+
+class ShipmentValidatorTest extends TestCase
+{
+    private $service;
+    private $recipientAddress;
+    private $senderAddress;
+    private $shop;
+    private $contract;
+    /** @var Shipment */
+    private $shipment;
+    private $weight;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->weight = 24;
+        $this->service = $this->getMockBuilder(Service::class)->getMock();
+        $this->recipientAddress = $this->getMockBuilder(Address::class)->getMock();
+        $this->senderAddress = $this->getMockBuilder(Address::class)->getMock();
+        $this->shop = $this->getMockBuilder(Shop::class)->getMock();
+        $this->contract = $this->getMockBuilder(Contract::class)->getMock();
+        $this->shipment = new Shipment();
+    }
+
+    /** @test */
+    public function testIsValid()
+    {
+        $shipment = (new Shipment())
+            ->setWeight($this->weight)
+            ->setService($this->service)
+            ->setRecipientAddress($this->recipientAddress)
+            ->setSenderAddress($this->senderAddress)
+            ->setShop($this->shop)
+            ->setContract($this->contract);
+
+        $validator = new ShipmentValidator($shipment);
+
+        $this->assertTrue($validator->isValid());
+    }
+
+    /** @test */
+    public function testMissingWeight()
+    {
+        $shipment = $this->createShipmentWithoutProperty('weight');
+
+        $validator = new ShipmentValidator($shipment);
+
+        $this->assertFalse($validator->isValid());
+    }
+
+    /** @test */
+    public function testMissingService()
+    {
+        $shipment = $this->createShipmentWithoutProperty('service');
+
+        $validator = new ShipmentValidator($shipment);
+
+        $this->assertFalse($validator->isValid());
+    }
+
+    /** @test */
+    public function testMissingRecipientAddress()
+    {
+        $shipment = $this->createShipmentWithoutProperty('recipient_address');
+
+        $validator = new ShipmentValidator($shipment);
+
+        $this->assertFalse($validator->isValid());
+    }
+
+    /** @test */
+    public function testMissingSenderAddress()
+    {
+        $shipment = $this->createShipmentWithoutProperty('sender_address');
+
+        $validator = new ShipmentValidator($shipment);
+
+        $this->assertFalse($validator->isValid());
+    }
+
+    /** @test */
+    public function testMissingShop()
+    {
+        $shipment = $this->createShipmentWithoutProperty('shop');
+
+        $validator = new ShipmentValidator($shipment);
+
+        $this->assertFalse($validator->isValid());
+    }
+
+    /** @test */
+    public function testMissingContract()
+    {
+        $shipment = $this->createShipmentWithoutProperty('contract');
+
+        $validator = new ShipmentValidator($shipment);
+
+        $this->assertFalse($validator->isValid());
+    }
+
+    /**
+     * Sets $this->shipment model with all the required properties
+     * except the given property.
+     *
+     * @param $missingProperty
+     * @return Shipment
+     */
+    private function createShipmentWithoutProperty($missingProperty)
+    {
+        $missingProperty = StringUtils::snakeToCamelCase($missingProperty);
+
+        $shipment = new Shipment();
+
+        $requiredProperties = ['weight', 'service', 'recipient_address', 'sender_address', 'shop', 'contract'];
+
+        foreach ($requiredProperties as $requiredProperty) {
+            $requiredProperty = StringUtils::snakeToCamelCase($requiredProperty);
+
+            if ($missingProperty !== $requiredProperty) {
+                $setter = 'set' . ucfirst($requiredProperty);
+                $shipment->$setter($this->$requiredProperty);
+            }
+        }
+
+        return $shipment;
+    }
+}
