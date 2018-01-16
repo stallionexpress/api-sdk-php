b3c18576e09d727cf1a507b365baaa135f607652
Nick de Vries <nick@myparcel.com>
2018-01-16 16:29:12 +0100

    :construction: Working on RegionProxyTest.

---
+++ tests/Feature/Proxy/RegionProxyTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MyParcelCom\ApiSdk\Tests\Feature\Proxy;
+
+use GuzzleHttp\ClientInterface;
+use MyParcelCom\ApiSdk\Authentication\AuthenticatorInterface;
+use MyParcelCom\ApiSdk\MyParcelComApi;
+use MyParcelCom\ApiSdk\MyParcelComApiInterface;
+use MyParcelCom\ApiSdk\Resources\Proxy\RegionProxy;
+use MyParcelCom\ApiSdk\Tests\Traits\MocksApiCommunication;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Cache\Simple\NullCache;
+
+class RegionProxyTest extends TestCase
+{
+    use MocksApiCommunication;
+
+    /** @var ClientInterface */
+    private $client;
+    /** @var AuthenticatorInterface */
+    private $authenticator;
+    /** @var MyParcelComApiInterface */
+    private $api;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->client = $this->getClientMock();
+        $this->authenticator = $this->getAuthenticatorMock();
+        $this->api = (new MyParcelComApi('https://api'))
+            ->setCache(new NullCache())
+            ->setHttpClient($this->client)
+            ->authenticate($this->authenticator);
+    }
+
+    /** @test */
+    public function testAttributes()
+    {
+        $regionProxy = new RegionProxy();
+        $regionProxy
+            ->setMyParcelComApi($this->api)
+            ->setId('c1048135-db45-404e-adac-fdecd0c7134a');
+    }
+}
