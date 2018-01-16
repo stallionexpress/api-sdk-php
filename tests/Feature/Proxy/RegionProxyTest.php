5c82ffc2a63825ac0fe673f2b823d8b096dd820c
Nick de Vries <nick@myparcel.com>
2018-01-16 16:45:51 +0100

    :white_check_mark: Finished writing the RegionProxyTest.

--- tests/Feature/Proxy/RegionProxyTest.php
+++ tests/Feature/Proxy/RegionProxyTest.php
@@ -6,6 +6,7 @@ use GuzzleHttp\ClientInterface;
 use MyParcelCom\ApiSdk\Authentication\AuthenticatorInterface;
 use MyParcelCom\ApiSdk\MyParcelComApi;
 use MyParcelCom\ApiSdk\MyParcelComApiInterface;
+use MyParcelCom\ApiSdk\Resources\Interfaces\ResourceInterface;
 use MyParcelCom\ApiSdk\Resources\Proxy\RegionProxy;
 use MyParcelCom\ApiSdk\Tests\Traits\MocksApiCommunication;
 use PHPUnit\Framework\TestCase;
@@ -41,5 +42,25 @@ class RegionProxyTest extends TestCase
         $regionProxy
             ->setMyParcelComApi($this->api)
             ->setId('c1048135-db45-404e-adac-fdecd0c7134a');
+
+        $this->assertEquals('c1048135-db45-404e-adac-fdecd0c7134a', $regionProxy->getId());
+        $this->assertEquals(ResourceInterface::TYPE_REGION, $regionProxy->getType());
+        $this->assertEquals('GB', $regionProxy->getCountryCode());
+        $this->assertEquals('ENG', $regionProxy->getRegionCode());
+        $this->assertEquals('GBP', $regionProxy->setCurrency('GBP')->getCurrency());
+        $this->assertEquals('United Kingdom', $regionProxy->getName());
+
+        // Check if the uri has been called only once.
+        // Creating a new proxy for the same resource will
+        // change the amount of calls to 2.
+        $this->assertEquals(1, $this->clientCalls['https://api/v1/regions/c1048135-db45-404e-adac-fdecd0c7134a']);
+
+        $newProxy = new RegionProxy();
+        $newProxy
+            ->setMyParcelComApi($this->api)
+            ->setId('c1048135-db45-404e-adac-fdecd0c7134a');
+        $newProxy->getName();
+
+        $this->assertEquals(2, $this->clientCalls['https://api/v1/files/c1048135-db45-404e-adac-fdecd0c7134a']);
     }
 }
