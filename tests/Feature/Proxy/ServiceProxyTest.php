53b077f3d954b6c965f297147b98fa4973d0e3bc
Nick de Vries <nick@myparcel.com>
2018-01-19 16:26:06 +0100

    :construction: Working on ServiceProxyTest and added stub for specific service.

--- tests/Feature/Proxy/ServiceProxyTest.php
+++ tests/Feature/Proxy/ServiceProxyTest.php
@@ -6,6 +6,9 @@ use GuzzleHttp\ClientInterface;
 use MyParcelCom\ApiSdk\Authentication\AuthenticatorInterface;
 use MyParcelCom\ApiSdk\MyParcelComApi;
 use MyParcelCom\ApiSdk\MyParcelComApiInterface;
+use MyParcelCom\ApiSdk\Resources\Interfaces\CarrierInterface;
+use MyParcelCom\ApiSdk\Resources\Interfaces\RegionInterface;
+use MyParcelCom\ApiSdk\Resources\Interfaces\ResourceInterface;
 use MyParcelCom\ApiSdk\Resources\Proxy\ServiceProxy;
 use MyParcelCom\ApiSdk\Tests\Traits\MocksApiCommunication;
 use PHPUnit\Framework\TestCase;
@@ -38,12 +41,44 @@ class ServiceProxyTest extends TestCase
         $this->serviceProxy = new ServiceProxy();
         $this->serviceProxy
             ->setMyParcelComApi($this->api)
-            ->setId('service-id-1');
+            ->setId('433285bb-2e34-435c-9109-1120e7c4bce4');
     }
 
     /** @test */
     public function testAttributes()
     {
-        $this->assertEquals();
+        $this->assertEquals('433285bb-2e34-435c-9109-1120e7c4bce4', $this->serviceProxy->getId());
+        $this->assertEquals(ResourceInterface::TYPE_SERVICE, $this->serviceProxy->getType());
+        $this->assertEquals('Letterbox Test', $this->serviceProxy->getName());
+        $this->assertEquals('letterbox', $this->serviceProxy->getPackageType());
+        $this->assertEquals(4, $this->serviceProxy->setTransitTimeMin(4)->getTransitTimeMin());
+        $this->assertEquals(3, $this->serviceProxy->getTransitTimeMax());
+    }
+
+    /** @test */
+    public function testCarrierRelationship()
+    {
+        $carrier = $this->serviceProxy->getCarrier();
+        $this->assertInstanceOf(CarrierInterface::class, $carrier);
+        $this->assertEquals('eef00b32-177e-43d3-9b26-715365e4ce46', $carrier->getId());
+        $this->assertEquals(ResourceInterface::TYPE_CARRIER, $carrier->getType());
+    }
+
+    /** @test */
+    public function testRegionFromRelationship()
+    {
+        $regionFrom = $this->serviceProxy->getRegionFrom();
+        $this->assertInstanceOf(RegionInterface::class, $regionFrom);
+        $this->assertEquals('c1048135-db45-404e-adac-fdecd0c7134a', $regionFrom->getId());
+        $this->assertEquals(ResourceInterface::TYPE_REGION, $regionFrom->getType());
+    }
+
+    /** @test */
+    public function testRegionToRelationship()
+    {
+        $regionTo = $this->serviceProxy->getRegionTo();
+        $this->assertInstanceOf(RegionInterface::class, $regionTo);
+        $this->assertEquals('c1048135-db45-404e-adac-fdecd0c7134a', $regionTo->getId());
+        $this->assertEquals(ResourceInterface::TYPE_REGION, $regionTo->getType());
     }
 }
