b7e8d5d39c32223cbca463b918b4bc5835138ba1
Nick de Vries <nick@myparcel.com>
2018-01-19 14:51:46 +0100

    :construction: Working on ServiceProxyTest and added stub for specific service.

---
+++ tests/Feature/Proxy/ServiceProxyTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace MyParcelCom\ApiSdk\Tests\Feature\Proxy;
+
+use GuzzleHttp\ClientInterface;
+use MyParcelCom\ApiSdk\Authentication\AuthenticatorInterface;
+use MyParcelCom\ApiSdk\MyParcelComApi;
+use MyParcelCom\ApiSdk\MyParcelComApiInterface;
+use MyParcelCom\ApiSdk\Resources\Proxy\ServiceProxy;
+use MyParcelCom\ApiSdk\Tests\Traits\MocksApiCommunication;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Cache\Simple\NullCache;
+
+class ServiceProxyTest extends TestCase
+{
+    use MocksApiCommunication;
+
+    /** @var ClientInterface */
+    private $client;
+    /** @var AuthenticatorInterface */
+    private $authenticator;
+    /** @var MyParcelComApiInterface */
+    private $api;
+    /** @var ServiceProxy */
+    private $serviceProxy;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->client = $this->getClientMock();
+        $this->authenticator = $this->getAuthenticatorMock();
+        $this->api = (new MyParcelComApi('https://api'))
+            ->setCache(new NullCache())
+            ->setHttpClient($this->client)
+            ->authenticate($this->authenticator);
+
+        $this->serviceProxy = new ServiceProxy();
+        $this->serviceProxy
+            ->setMyParcelComApi($this->api)
+            ->setId('service-id-1');
+    }
+
+    /** @test */
+    public function testAttributes()
+    {
+        $this->assertEquals();
+    }
+}
