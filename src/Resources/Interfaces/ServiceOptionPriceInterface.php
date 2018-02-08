3459dffd139d4dfdad6418868de6bd25f5a7a2d4
Shady Khattab <shady@myparcel.com>
2018-02-08 12:11:06 +0100

    ♻️ Updated Interfaces to reflect new contracts structure

---
+++ src/Resources/Interfaces/ServiceOptionPriceInterface.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace MyParcelCom\ApiSdk\Resources\Interfaces;
+
+interface ServiceOptionPriceInterface extends ResourceInterface
+{
+    /**
+     * @param string $id
+     * @return $this
+     */
+    public function setId($id);
+
+    /**
+     * @param int $price
+     * @return $this
+     */
+    public function setPrice($price);
+
+    /**
+     * @return int
+     */
+    public function getPrice();
+
+    /**
+     * @param string $currency
+     * @return $this
+     */
+    public function setCurrency($currency);
+
+    /**
+     * @return string
+     */
+    public function getCurrency();
+
+    /**
+     * @param ServiceContractInterface $serviceContract
+     * @return $this
+     */
+    public function setServiceContract(ServiceContractInterface $serviceContract);
+
+    /**
+     * @return ServiceContractInterface
+     */
+    public function getServiceContract();
+
+    /**
+     * @param ServiceOptionInterface $serviceOption
+     * @return $this
+     */
+    public function setServiceOption(ServiceOptionInterface $serviceOption);
+
+    /**
+     * @return ServiceOptionInterface
+     */
+    public function getServiceOption();
+}
