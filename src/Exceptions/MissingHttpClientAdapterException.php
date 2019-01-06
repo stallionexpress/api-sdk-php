1c708bf42d212e6e0888195ba0fde9d030857014
Dennis <denniskoster90@gmail.com>
2019-01-06 16:36:42 +0100

    :memo: Add doc to reflect when exception is thrown

--- src/Exceptions/MissingHttpClientAdapterException.php
+++ src/Exceptions/MissingHttpClientAdapterException.php
@@ -2,6 +2,10 @@
 
 namespace MyParcelCom\ApiSdk\Exceptions;
 
+/**
+ * Thrown when an existing HTTP client was found, but
+ * the required adapter package has not been installed.
+ */
 class MissingHttpClientAdapterException extends \Exception
 {
 
